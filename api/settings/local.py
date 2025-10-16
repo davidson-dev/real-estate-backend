474590550679bcde7b0a9f1364bc0f9a71712fc3
Davidson Très <davidson.pydev@gmail.com>
2025-10-15 21:31:55 -0400

    feat: setup project

---
+++ api/settings/local.py
@@ -0,0 +1,10 @@
+from .base import *  # noqa
+from .base import env
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-p^5i#=^dl1((g66n-a0t#sfi!%^pm^l+!3x*uohqur0e0jf8l6"
+DEBUG = True
+ALLOWED_HOSTS = []
